af298ff34dbfe2e45784b5172c2b12161129e1a5
wescottsharples <wesc@ttsharpl.es>
2018-09-12 20:06:13 +0000

    Making file 7 conform to betty style and betty doc guidelines

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char c;
@@ -11,9 +16,9 @@ int main(void)
 	printf("The size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("The size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
 	printf("The size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
-	printf("The size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
+	printf("The size of a long long int: %l byte(s)\n", (long)sizeof(lli));
 	printf("The size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 
 
-	return(0);
+	return (0);
 }
