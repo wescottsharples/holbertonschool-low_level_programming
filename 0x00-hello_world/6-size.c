089248475c7c38cbdcf37cd659579523b43ae3fb
wescottsharples <wesc@ttsharpl.es>
2018-09-12 20:12:31 +0000

    Changing C program to use integer type instead of unsigned long

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ int main(void)
 	long long int lli;
 	float f;
 
-	printf("The size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("The size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
-	printf("The size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
-	printf("The size of a long long int: %l byte(s)\n", (long)sizeof(lli));
-	printf("The size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf("The size of a char: %i byte(s)\n", (int)sizeof(c));
+	printf("The size of an int: %i byte(s)\n", (int)sizeof(i));
+	printf("The size of a long int: %i byte(s)\n", (int)sizeof(li));
+	printf("The size of a long long int: %i byte(s)\n", (int)sizeof(lli));
+	printf("The size of a float: %i byte(s)\n", (int)sizeof(f));
 
 
 	return (0);
