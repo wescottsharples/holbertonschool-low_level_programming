7e500d4e8d406a26d44ed7c3adeb5f1d822c95a3
wescottsharples <wesc@ttsharpl.es>
2018-09-13 22:14:44 +0000

    Changed script to check the value of the last digit instead of the number itself in my if else statement

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,15 +19,15 @@ int main(void)
 	printf("Last digit of %d is ", n);
 	printf("%d", n % 10);
 
-	if (n < 6 && n != 0)
+	if ((n % 10) < 6 && (n % 10) != 0)
 	{
 		printf(" and is less than 6 and not 0\n");
 	}
-	if (n > 5)
+	if ((n % 10) > 5)
 	{
 		printf(" and is greater than 5\n");
 	}
-	if (n == 0)
+	if ((n % 10) == 0)
 	{
 		printf(" and is 0\n");
 	}
