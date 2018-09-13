f98591bbae61c13d2d95000997a9f6d2e2c6fef0
wescottsharples <wesc@ttsharpl.es>
2018-09-13 21:19:09 +0000

    Adding a C program that prints numbers 0 through 99 in two digit format seperated by a space and comma, only using putchar function

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0 ; num <= 99 ; num++)
+	{
+		putchar((num / 10) + '0');
+		putchar((num % 10) + '0');
+
+		if (num < 99)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		else
+		{
+		continue;
+		}
+	}
+
+	putchar("/n");
+
+	return (0);
+}
