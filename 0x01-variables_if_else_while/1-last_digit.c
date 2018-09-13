c59980dc1ff58d04c316f79ee2aca76d30340f72
wescottsharples <wesc@ttsharpl.es>
2018-09-13 22:10:54 +0000

    Adding a C program that makes use of the modulus operator to print the last digit of a random number and specify if it is either less than 6, equal to 0, or greater than 5

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,31 +4,34 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	/* your code goes there */
-	while ((c = getchar()) = EOF)
-		putchar(c);
+	printf("Last digit of %d is ", n);
+	printf("%d", n % 10);
 
-	printf("Last digit of %d is %d", n);
-
+	if (n < 6 && n != 0)
+	{
+		printf(" and is less than 6 and not 0\n");
+	}
 	if (n > 5)
 	{
-		printf("and is greater than 5\n");
+		printf(" and is greater than 5\n");
 	}
-	else if (n == 0)
+	if (n == 0)
 	{
-		printf("and is 0\n");
-	} 
-	else if (n < 6 && n != 0)
-	{
-		printf("and is less than 6 and not 0\n");
-	} else
+		printf(" and is 0\n");
+	}
+	else
 	{
 	}
 
