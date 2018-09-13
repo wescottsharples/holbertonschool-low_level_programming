f72a74418bf37b948f6cf022ae44373b0a3e5101
wescottsharples <wesc@ttsharpl.es>
2018-09-13 19:35:35 +0000

    Adding a C program that prints the alphabet using putchar and doesn't print q or e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a' ; alpha <= 'z' ; alpha++)
+	{
+		if (alpha != 'e' && alpha != 'q')
+		{
+			putchar(alpha);
+		}
+		else
+		{
+			continue;
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
