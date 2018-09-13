43f35a7d9b0217d6019202ecaf417b864583ba74
wescottsharples <wesc@ttsharpl.es>
2018-09-13 19:22:25 +0000

    Adding a C program that prints the alphabet using only putchar

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	/* your code goes there */
+	while ((c = getchar()) = EOF)
+		putchar(c);
+
+	printf("Last digit of %d is %d", n);
+
+	if (n > 5)
+	{
+		printf("and is greater than 5\n");
+	}
+	else if (n == 0)
+	{
+		printf("and is 0\n");
+	} 
+	else if (n < 6 && n != 0)
+	{
+		printf("and is less than 6 and not 0\n");
+	} else
+	{
+	}
+
+	return (0);
+}
