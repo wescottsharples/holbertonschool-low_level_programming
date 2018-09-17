c56c84836cd203d77b55acb7e6625e9c94cb5675
wescottsharples <wesc@ttsharpl.es>
2018-09-17 18:02:02 +0000

    Adding a header file titled holberton that will contain all of my function prototypes

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
  * main - check the code for Holberton School students.
@@ -7,6 +7,15 @@
  */
 int main(void)
 {
-	printf("Holberton\n");
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 	return (0);
 }
