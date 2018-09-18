4ef14333afa232a7bf3986a79d472b9c9ab3737c
wescottsharples <wesc@ttsharpl.es>
2018-09-18 20:30:22 +0000

    Changed function to check if a character is alpha to include holberton.h file in header and describe what the variable does

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,8 @@
+#include "holberton.h"
+
 /**
  * _isalpha - checks is the input is an alphabetical character.
- *
+ * @c: variable to check if the character is alpha
  * Return: Always 1 or 0.
  */
 int _isalpha(int c)
