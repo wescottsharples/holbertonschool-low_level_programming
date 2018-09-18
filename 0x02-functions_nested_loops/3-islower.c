2746f5b37f2147d365de1837266cb8d54524da12
wescottsharples <wesc@ttsharpl.es>
2018-09-18 20:28:10 +0000

    Changed is lower function to include holberton.h file in header and commented what the variable c does in the function

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,8 @@
+#include "holberton.h"
+
 /**
  * _islower - checks if the input is a lowercase character.
- *
+ * @c: Integer that keeps track of character we are checking.
  * Return: Always 1 or 0.
  */
 int _islower(int c)
