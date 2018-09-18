69524594f423265591f8ea83027bd6b5a6f727e6
wescottsharples <wesc@ttsharpl.es>
2018-09-18 22:20:52 +0000

    Changed add function to conform to betty standards with a description of what the function's parameters do

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,5 +1,11 @@
 #include "holberton.h"
 
+/**
+ * add - adds two integers.
+ * @a: first integer to be added
+ * @b: second integer to be added
+ * Return: Character.
+ */
 int add(int a, int b)
 {
 	int c;
