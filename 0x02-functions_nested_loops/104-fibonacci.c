615e2acf488c21fad44c2358353b4ffb6cea5f53
wescottsharples <wesc@ttsharpl.es>
2018-09-19 06:06:05 +0000

    Adding a program that prints the first 98 terms in the Fibonacci sequence

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,47 +1,46 @@
 #include <stdio.h>
 
 /**
- * fibonacci - prints the first 98 terms in the fibonacci sequence.
+ * main - Prints the fibonacci sequence for 98 terms
  *
- * Return: Nothing.
- */
-void fibonacci(void)
-{
-	int i;
-	unsigned long long a = 1;
-	unsigned long long b = 2;
-
-	printf("%llu, %llu, ", a, b);
-
-	for (i = 0; i < 96; i++)
-	{
-		if (a < b)
-		{
-			a = a + b;
-			printf("%llu", a);
-		}
-		else if (a > b)
-		{
-			b = b + a;
-			printf("%llu", b);
-		}
-
-		if (i != 95)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
-}
-
-/**
- * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	fibonacci();
+	/*Declaring variables*/
+	unsigned long total1, total2, a1 = 1, b1 = 2, a2 = 0, b2 = 0;
+	int i, overflow;
+	/*Printing the first two numbers*/
+	printf("%li, %li, ", a1, b1);
+	/*Breaking the numbers into parts*/
+	for (i = 0; i < 96; i++)
+	{
+		total1 = a1 + b1;
+		if ((total1 / 100000000000000) > 0)
+		{
+			overflow = total1 / 100000000000000;
+			total1 = total1 % 100000000000000;
+		}
+		/*Printing the numbers by combining constituents*/
+		total2 = a2 + b2 + overflow;
+		if (total2 > 0)
+			printf("%li%li", total2, total1);
+		else
+			printf("%li", total1);
+		/*Setting my variables for next iteration*/
+		overflow = 0;
+		a1 = b1;
+		a2 = b2;
+		b1 = total1;
+		b2 = total2;
+		/*Print comma and spaces*/
+		if (i < 95)
+		{
+			printf(", ");
+		}
+	}
+
 	putchar('\n');
 	return (0);
 }
