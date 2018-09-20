1c4fec3c8b74eb614d80589cf2765559b26356d1
wescottsharples <wesc@ttsharpl.es>
2018-09-20 19:53:50 +0000

    Add a program that prints 1-100 replacing multiples of 3 and 5

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - prints 1-100 replacing multiples of 3 and 5
+ * with the text Fizz and Buzz and FizzBuzz
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 != 0))
+		{
+			printf("Fizz");
+		}
+		else if ((i % 5 == 0) && (i % 3 != 0))
+		{
+			printf("Buzz");
+		}
+		else if ((i % 3 == 0) && (i % 3 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+
+		if (i < 100)
+			printf(" ");
+	}
+
+	return (0);
+}
