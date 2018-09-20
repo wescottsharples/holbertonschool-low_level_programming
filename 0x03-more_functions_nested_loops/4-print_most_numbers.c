56e757107b0ba3fe284bd23559bffa5faa3e65fc
wescottsharples <wesc@ttsharpl.es>
2018-09-20 18:15:12 +0000

    Add description of function and new line after variable declaration

--- 0x03-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,14 @@
 #include "holberton.h"
 
+/**
+ * print_most_numbers - prints numbers 1-9 without 2 or 4.
+ *
+ * Return: Always 0.
+ */
 void print_most_numbers(void)
 {
 	int i;
+
 	for (i = '0'; i <= '9'; i++)
 	{
 		if (i != '2' && i != '4')
