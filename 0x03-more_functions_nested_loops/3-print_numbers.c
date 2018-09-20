e0eaee59f5b207151fe26df543f5a884524abfac
wescottsharples <wesc@ttsharpl.es>
2018-09-20 18:13:19 +0000

    Add description of function and new line under variable declaration

--- 0x03-more_functions_nested_loops/3-print_numbers.c
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,14 @@
 #include "holberton.h"
 
+/**
+ * print_numbers - prints numbers 1-9.
+ *
+ * Return: Always 0.
+ */
 void print_numbers(void)
 {
 	int i;
+
 	for (i = '0'; i <= '9'; i++)
 		_putchar(i);
 
