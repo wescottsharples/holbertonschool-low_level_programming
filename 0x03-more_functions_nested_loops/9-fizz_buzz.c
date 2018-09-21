8dfb6ca9ede0529d6bac17319623f65c08b0a143
wescottsharples <wesc@ttsharpl.es>
2018-09-21 03:21:05 +0000

    Add new line

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 101; i++)
+	for (i = 1; i < 101; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 != 0))
 		{
@@ -31,5 +31,6 @@ int main(void)
 				printf(" ");
 	}
 
+	putchar('\n');
 	return (0);
 }
