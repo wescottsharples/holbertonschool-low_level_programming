e5dc7dae9806b580c873220189059cd318fe5df0
wescottsharples <wesc@ttsharpl.es>
2018-09-24 18:38:48 +0000

    Add function that prints a string to standard output

---
+++ 0x04-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - updates the value of n's variable to 98.
+ *
+ * Return: None.
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; i == '\0'; i++)
+	{
+		_putchar(*str[i]);
+	}
+
+	_putchar('\n');
+}
