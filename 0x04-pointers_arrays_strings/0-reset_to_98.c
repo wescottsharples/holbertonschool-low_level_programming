b5a56d683d84e9a784aa28c4e507bbb3b0ae1948
wescottsharples <wesc@ttsharpl.es>
2018-09-24 18:21:13 +0000

    Add function that updates pointer n's value to 98

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - updates the value of n's variable to 98.
+ * @n: pointer that has an unknown value
+ * Return: None.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
