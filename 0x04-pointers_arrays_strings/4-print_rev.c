e3730a8c68df0b208d66defd7765f248e44030e8
wescottsharples <wesc@ttsharpl.es>
2018-09-24 18:53:38 +0000

    Add program that prints a string in reverse

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - updates the value of n's variable to 98.
+ *
+ * Return: None.
+ */
+void print_rev(char *s)
+{
+	int string_size, string_length;
+	int i;
+
+	string_size = sizeof(*s);
+	string_length = (string_size / 1) - 1;
+
+	for (i = size_length; i >= 0; i--)
+	{
+		_putchar(*s[i]);
+	}
+}
