2b4e093690851294f6d7b8accf3b4320ebd37362
wescottsharples <wesc@ttsharpl.es>
2018-09-24 18:33:49 +0000

    Add program that returns length of string

---
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - updates the value of n's variable to 98.
+ *
+ * Return: None.
+ */
+int _strlen(char *s)
+{
+	int string_size, string_length;
+
+	string_size = sizeof(*s);
+	string_length = (string_size / 1) - 1;
+	return string_length;
+}
