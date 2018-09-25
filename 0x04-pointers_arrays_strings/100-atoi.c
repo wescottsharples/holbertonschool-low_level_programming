14955ab49bfcb9036f8f78d09394c7f14885dd47
wescottsharples <wesc@ttsharpl.es>
2018-09-25 19:40:35 +0000

    Add program that converts string to int

---
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _atoi - converts a string into an integer.
+ * @s: the string to be converted
+ * Return: Returns the integer from string
+ * or 0 if no integers were found.
+ */
+int _atoi(char *s)
+{
+	int final = 0;
+	int norp = 1;
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			norp = norp * -1;
+		}
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			final = (final * 10) + (s[i] - '0');
+
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+			{
+			break;
+			}
+		}
+
+		i++;
+	}
+
+	return (norp*final);
+}
