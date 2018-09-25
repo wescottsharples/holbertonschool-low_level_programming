5752ddabf06547b99fdb49e9ad7bfe1377889e8d
wescottsharples <wesc@ttsharpl.es>
2018-09-25 02:46:06 +0000

    Fix calculation of string length

---
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * puts_half - prints the latter half of a string.
+ * @str: the string you want to print half of
+ * Return: None.
+ */
+void puts_half(char *str)
+{
+	int i;
+
+	while (*(str + i) != '\0')
+		i++;
+
+	i = (i / 2);
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
