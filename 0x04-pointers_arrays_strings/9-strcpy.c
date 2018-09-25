95599302dce57dde2476c35f5d287ac26c5d37f0
wescottsharples <wesc@ttsharpl.es>
2018-09-25 03:08:45 +0000

    Add program that copies a string

---
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - copties a string to another string.
+ * @dest: the string you want to copy to
+ * @src: the string that you want to copy from
+ * Return: The pointer to dest string.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
