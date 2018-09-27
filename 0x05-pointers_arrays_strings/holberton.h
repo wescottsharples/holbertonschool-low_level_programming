78fbc1e30fd8a50d4f241ab4f7de0a88d41b7721
wescottsharples <wesc@ttsharpl.es>
2018-09-27 23:27:57 +0000

    Add header file with function prototypes

---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+char *_strcat(char *dest, char *src);
+char *string_toupper(char *);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+
+#endif
+
