6482a683e387e6a2a2db30c911fc624ae81e40a6
wescottsharples <wesc@ttsharpl.es>
2018-09-28 06:53:32 +0000

    Add skeleton for function infinate add

---
+++ 0x05-pointers_arrays_strings/102-infinite_add.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * infinate_add - adds infinately.
+ * @n2: the first add
+ * @1: the second add
+ * @r: the array to be added
+ * @size_r: the size of r
+ * Return: An array.
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	n1;
+	n2;
+	r;
+	size_r;
+
+	return (n1);
+}
