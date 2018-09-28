c70671d373fed7b192f3bf17d2cf325005636e2c
wescottsharples <wesc@ttsharpl.es>
2018-09-28 05:52:11 +0000

    Fix returns difference instead of fixed number

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -16,9 +16,9 @@ int _strcmp(char *s1, char *s2)
 		i++;
 
 	if ((s1[i] - s2[i]) > 0)
-		return (15);
+		return (s1[i] - s2[i]);
 	else if ((s2[i] - s1[i]) > 0)
-		return (-15);
+		return (s1[i] - s2[i]);
 	else
 		return (0);
 }
