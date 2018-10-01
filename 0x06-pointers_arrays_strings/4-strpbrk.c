2526b6c58297311f14ad399a7c7c0d0dc6e6bf6a
wescottsharples <wesc@ttsharpl.es>
2018-10-01 21:49:37 +0000

    Function that locates first occurence in 's' of char set

---
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _strpbrk - locates first occurence of char set in 's'.
+ * @s: the string to find matches in
+ * @accept: the string to be matched to
+ * Return: Pointer to first byte that matches.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; *(accept + j); j++)
+		{
+			if (*(s + i) == *(accept + j))
+				return (s + i);
+		}
+	}
+
+	return (s + i);
+}
