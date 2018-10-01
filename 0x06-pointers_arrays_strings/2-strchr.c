35d84ea1c8df123aea0e65d0e903904a122462d0
wescottsharples <wesc@ttsharpl.es>
2018-10-01 20:38:39 +0000

    Fix iterate through NULL value

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -12,14 +12,11 @@ char *_strchr(char *s, char c)
 {
 	unsigned int i;
 
-	for (i = 0; *(s + i); i++)
+	for (i = 0; *(s + i) <= '\0'; i++)
 	{
 		if (*(s + i) == c)
 			return (s + i);
 	}
 
-	if (*(s + i) ==  '\0')
-		return (s + i);
-	else
-		return (0);
+	return (0);
 }
