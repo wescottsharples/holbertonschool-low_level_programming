de1c9fca323564f2ed6ad096c248785409b4c371
wescottsharples <wesc@ttsharpl.es>
2018-10-01 19:04:16 +0000

    Function which writes over n bytes

---
+++ 0x06-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _memset - replaces n bytes in the memory
+ * area pointed to by s with the constant b
+ * @s: the memory address to be filled
+ * @b: the constant byte to fill with 
+ * @n: n bytes of memory to be filled
+ * Return: A pointer to the memory area 's'.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(s + i) = b;
+
+	return (s);
+}
