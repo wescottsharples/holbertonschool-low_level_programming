2ba93c7783523f1032a2b8bb194367bf29b4845e
wescottsharples <wesc@ttsharpl.es>
2018-10-01 20:01:21 +0000

    Add header file with function prototypes

---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,9 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+
+#endif
