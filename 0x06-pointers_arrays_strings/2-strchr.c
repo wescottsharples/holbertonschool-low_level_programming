e4a1e6d10c7398500ad4c545b293fbab08be82e3
wescottsharples <wesc@ttsharpl.es>
2018-10-01 19:47:34 +0000

    Function that finds first instance of a character in string

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _strchr - finds the first instance of char 'c'
+ * in string 's'
+ * @s: the string to be searched
+ * @c: the character searched for
+ * Return: Pointer to first instance of 'c' in 's'
+ * or NULL if no instances are found.
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0; *(s + i); i++)
+	{
+		if (*(s + i) == c)
+			return (s + i);
+	}
+
+	return (0);
+}
