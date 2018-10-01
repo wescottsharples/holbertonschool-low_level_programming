c6bb18c350a9682db81ca167a0f6efcc7e8dfc38
wescottsharples <wesc@ttsharpl.es>
2018-10-01 21:57:39 +0000

    Fix returning null character

--- 0x06-pointers_arrays_strings/4-strpbrk.c
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -10,7 +10,7 @@ char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
 
-	for (i = 0; s[i] >= '\0'; i++)
+	for (i = 0; *(s + i); i++)
 	{
 		for (j = 0; *(accept + j); j++)
 		{
@@ -19,5 +19,5 @@ char *_strpbrk(char *s, char *accept)
 		}
 	}
 
-	return (0);
+	return ('\0');
 }
