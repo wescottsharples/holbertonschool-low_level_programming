4a4d110380fcf7293df114ae002793fde5c5bf6a
wescottsharples <wesc@ttsharpl.es>
2018-10-03 23:06:20 +0000

    Add description of return values

--- 0x07-recursion/4-pow_recursion.c
+++ 0x07-recursion/4-pow_recursion.c
@@ -1,9 +1,12 @@
 #include "holberton.h"
 
-/*
+/**
  * _pow_recursion - raises x to the power of y
  * @x: the base to be raised
  * @y: the power to raise to
+ * Return: Result if y > 0
+ *         1 if y == 0
+ *	  -1 if y < 0
  */
 int _pow_recursion(int x, int y)
 {
