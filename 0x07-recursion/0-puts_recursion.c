6248ed727eaff7356787b5350690537cecc83f23
wescottsharples <wesc@ttsharpl.es>
2018-10-04 06:28:38 +0000

    Function prints a string using recursion

---
+++ 0x07-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: the string to be printed
+ * Return: Nothing.
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+
+	if (*s)
+		_puts_recursion(s + 1);
+	else
+		_putchar('\n');
+}
