63631983b13f5edc573a16d306ee88ca4f7d255f
wescottsharples <wesc@ttsharpl.es>
2018-10-06 21:30:08 +0000

    Fix description of main function

--- 0x09-argc_argv/1-args.c
+++ 0x09-argc_argv/1-args.c
@@ -2,14 +2,13 @@
 #include "holberton.h"
 
 /**
- * main - prints the program's name
+ * main - prints the number of arguments passed to it
  * @argc: the number of arguments to main
  * @argv: an arrary of string arguments
  * Return: Always 0.
  */
 int main(int argc, __attribute__((__unused__)) char *argv[])
 {
-	
 	printf("%d\n", argc);
 	return (0);
 }
