1032c0db29c1146c1317e470e3032826606a1565
wescottsharples <wesc@ttsharpl.es>
2018-10-06 21:25:36 +0000

    Program prints the number of arguments passed to it

---
+++ 0x09-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - prints the program's name
+ * @argc: the number of arguments to main
+ * @argv: an arrary of string arguments
+ * Return: Always 0.
+ */
+int main(int argc, __attribute__((__unused__)) char *argv[])
+{
+	
+	printf("%d\n", argc);
+	return (0);
+}
