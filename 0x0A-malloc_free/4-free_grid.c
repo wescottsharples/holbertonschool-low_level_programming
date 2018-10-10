17b8748761216d24ab30107cae7240269069167f
wescottsharples <wesc@ttsharpl.es>
2018-10-10 06:47:42 +0000

    Function frees 2D grid from memory

---
+++ 0x0A-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a 2D grid
+ * @grid: the grid to be freed
+ * @height: height of the grid
+ * Return: Nothing.
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL || height == 0)
+		return;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+
+	free(grid);
+}
