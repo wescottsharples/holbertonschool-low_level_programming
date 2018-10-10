82c58fad6c37055a0c5e6ba68b78a6706ce89baf
wescottsharples <wesc@ttsharpl.es>
2018-10-10 20:32:41 +0000

    Add new method of grid initialization

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -1,37 +1,46 @@
-#include "holberton.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
 
 /**
- * alloc_grid - allocates memory for a 2D array of integers
- * @width: size of the integer array
- * @height: size of the pointer array
- * Return: Pointer to the grid, else NULL.
+ * alloc_grid - allocates memory for a 2D array and initializes it to zero
+ * @width: the width of array
+ * @height: the height of array
+ * Return: Pointer to grid, else NULL.
  */
 int **alloc_grid(int width, int height)
 {
 	int i, j;
-	int **grid = malloc(sizeof(int *) * height);
+	int **grid;
 
-	if (width <= 0 || height <= 0)
+	if (height <= 0 || width <= 0)
+		return (NULL);
+
+	grid = malloc(sizeof(int *) * height);
+	if (!grid)
 		return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = (int *)malloc(sizeof(int) * width);
-		if (grid[i] == NULL)
+		grid[i] = malloc(sizeof(int) * width);
+		if (!grid[i])
 		{
-			j = i - 1;
-			break;
+			for (i = 0; i < height; i++)
+				free(grid[i]);
+			return (NULL);
 		}
 	}
 
-	if (j > 0)
+	for (i = 0; i < height; i++)
 	{
-		for (j = i - 1; j >= 0; j--)
-			free(grid[j]);
-		free(grid);
-		return (NULL);
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+
+		if (!grid[i])
+		{
+			free(grid);
+			return (NULL);
+		}
 	}
 
 	return (grid);
