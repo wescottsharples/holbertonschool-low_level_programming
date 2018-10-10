b6b95e735c00a4b6e75237e94277e51df7565d9c
wescottsharples <wesc@ttsharpl.es>
2018-10-10 05:01:50 +0000

    Function copies a string given as a parameter

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * _strdup - copy of the string given as a parameter
+ * @str: the string to be copied
+ * Return: NULL if string is NULL, else pointer to copied string
+ */
+char *_strdup(char *str)
+{
+	if (str == NULL)
+		return (NULL);
+
+	unsigned int len, i;
+
+	for (len = 0; str[len]; len++)
+		;
+
+	char *cpy = malloc(sizeof(char) * (len + 1));
+
+	for (i = 0; str[i]; i++)
+		cpy[i] = str[i];
+
+	return (cpy);
+}
