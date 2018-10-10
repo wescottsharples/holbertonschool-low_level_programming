eed3f13b30423123c25d69f7a327cca2ca93a35d
wescottsharples <wesc@ttsharpl.es>
2018-10-10 04:21:47 +0000

    Function creates string of specific char and size

---
+++ 0x0A-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * create_array - creates an array of specified 'size' initialized with 'c'
+ * @size: the size of the array
+ * @c: the character to initialize the array with
+ * Return: NULL if size is zero, else pointer to array
+ */
+char *create_array(unsigned int size, char c)
+{
+	if (!size)
+		return ('\0');
+
+	unsigned int i = 0;
+	char *str = (char *)malloc(sizeof(char) * size);
+
+	while (i < size)
+	{
+		str[i] = c;
+		i++;
+	}
+
+	return (str);
+}
