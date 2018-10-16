fc031a3a3065e86b58487651fec7ed62ec119020
wescottsharples <wesc@ttsharpl.es>
2018-10-16 00:45:29 +0000

    Header with struct about dog

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - information about dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
