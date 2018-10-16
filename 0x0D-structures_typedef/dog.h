56330daf2fda2db1b97853041a09d9dd2c1354da
wescottsharples <wesc@ttsharpl.es>
2018-10-16 01:44:43 +0000

    Add typedef dog_t for struct dog

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -7,12 +7,12 @@
  * @age: dog's age
  * @owner: dog's owner
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 /* Function prototypes */
 int _putchar(char c);
