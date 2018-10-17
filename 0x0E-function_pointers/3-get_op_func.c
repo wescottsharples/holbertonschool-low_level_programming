40d5574f27e47f833fd25a8f1f7393c005dd7f0c
wescottsharples <wesc@ttsharpl.es>
2018-10-17 22:24:08 +0000

    Function to find the correct function based on operator

---
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+#include "3-calc.h"
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+	int i = 0;
+
+	while (i < 6)
+	{
+		if (*s == ops[i].op[0])
+			return ops[i].f;
+		i++;
+	}
+
+	return (NULL);
+}
