efa47423c2785feb5cd22b716c923a8f17be615c
wescottsharples <wesc@ttsharpl.es>
2018-10-17 23:34:05 +0000

    Fix explicit state of NULL

--- 0x0E-function_pointers/0-print_name.c
+++ 0x0E-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <stdio.h>
 
 /**
  * print_name - prints a name using a function pointer
@@ -8,7 +7,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if ((!name) || (!f))
+	if ((name == 0) || (f == 0))
 		return;
 	f(name);
 }
