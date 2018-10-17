56e4b6b9850245667f5e0a89e749f8528086278d
wescottsharples <wesc@ttsharpl.es>
2018-10-17 22:47:10 +0000

    Fix betty style

--- 0x0E-function_pointers/3-op_functions.c
+++ 0x0E-function_pointers/3-op_functions.c
@@ -1,25 +1,55 @@
 #include "function_pointers.h"
 
+/**
+ * op_add - adds two ints
+ * @a: the first integer
+ * @b: the second integer
+ * Return: Sum of 'a' and 'b'.
+ */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
+/**
+ * op_sub - subtracts two ints
+ * @a: the first integer
+ * @b: the second integer
+ * Return: Difference of 'a' and 'b'.
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
+/**
+ * op_mul - multiplies two ints
+ * @a: the first integer
+ * @b: the second integer
+ * Return: Product of 'a' and 'b'.
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
+/**
+ * op_div - divides two ints
+ * @a: the first integer
+ * @b: the second integer
+ * Return: Quotient of 'a' and 'b'.
+ */
 int op_div(int a, int b)
 {
 	return (a / b);
 }
 
+/**
+ * op_mod - computes modulus of two ints
+ * @a: the first integer
+ * @b: the second integer
+ * Return: Modulus of 'a' and 'b'.
+ */
 int op_mod(int a, int b)
 {
 	return (a % b);
