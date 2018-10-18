f4037f13ab75a249e779029a8e8ebf758c61dff8
wescottsharples <wesc@ttsharpl.es>
2018-10-18 00:28:00 +0000

    Fix logic to check single char operator

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -13,7 +13,6 @@
 int main(int argc, char *argv[])
 {
 	int result, num1, num2;
-	char *op;
 
 	if (argc != 4)
 	{
@@ -23,21 +22,20 @@ int main(int argc, char *argv[])
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	op = argv[2];
 
-	if (num2 == 0 && (*op == '/' || *op == '%'))
+	if (num2 == 0 && (*argv[2] == '/' || *argv[2] == '%'))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	if (!get_op_func(op))
+	if (!get_op_func(argv[2]) || *(argv[2] + 1))
 	{
 		printf("Error\n");
 		return (99);
 	}
 
-	result = get_op_func(op)(num1, num2);
+	result = get_op_func(argv[2])(num1, num2);
 	printf("%d\n", result);
 	return (result);
 }
