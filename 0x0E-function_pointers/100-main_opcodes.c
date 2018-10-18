e942575360af85ed168cc299c55e6b879a2093fd
wescottsharples <wesc@ttsharpl.es>
2018-10-18 04:10:17 +0000

    Program that prints the opcodes of its own main function

---
+++ 0x0E-function_pointers/100-main_opcodes.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int foo(void);
+
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: the number of arguments passed at command line
+ * @argv: an array of argument strings passed at command line
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i, number_of_bytes;
+	unsigned char *opcode;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	number_of_bytes = atoi(argv[1]);
+
+	if (number_of_bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	opcode = (unsigned char *)(long int)main;
+
+	for (i = 0; i < number_of_bytes; i++)
+	{
+		printf("%x", *(opcode + i));
+
+		if (i < (number_of_bytes - 1))
+			putchar(' ');
+		else
+			putchar('\n');
+	}
+
+	return (0);
+}
