3b41c6c18c7a8c34f11ad7539dfe8ce1bfaaf7f3
wescottsharples <wesc@ttsharpl.es>
2018-10-26 06:24:06 +0000

    Split print statement onto two lines

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -10,6 +10,6 @@ void tortoise(void) __attribute__ ((constructor));
  */
 void tortoise(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-	I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+	"I bore my house upon my back!\n");
 }
