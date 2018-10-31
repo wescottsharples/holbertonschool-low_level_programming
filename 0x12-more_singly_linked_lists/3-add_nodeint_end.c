08884ec17189fedc36ee686d8d790ba95af9886d
wescottsharples <wesc@ttsharpl.es>
2018-10-31 05:00:41 +0000

    Fix malloc of struct

--- 0x12-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x12-more_singly_linked_lists/3-add_nodeint_end.c
@@ -21,7 +21,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 			last = last->next;
 	}
 
-	new = malloc(sizeof(size_t));
+	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
 
