6049fa6340492ab48be219e787651bc9b8a39ea0
wescottsharples <wesc@ttsharpl.es>
2018-10-31 05:10:56 +0000

    Add NULL check

--- 0x12-more_singly_linked_lists/2-add_nodeint.c
+++ 0x12-more_singly_linked_lists/2-add_nodeint.c
@@ -18,9 +18,13 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	if (!new)
 		return (NULL);
 
-	new->n = n;
-	new->next = *head;
-	*head = new;
+	if (*head && head)
+	{
+		new->n = n;
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 
-	return (new);
+	return (NULL);
 }
