7751e55a7c6209252f148f990407eb0869e1eaae
wescottsharples <wesc@ttsharpl.es>
2018-11-01 19:37:44 +0000

    Function converts binary to decimal

---
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - function converts binary number to unsigned int
+ * @b: pointer to binary number in string format
+ *
+ * Return: Converted number, else 0.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i, mul, sum;
+
+	i = 0;
+	mul = 0;
+	sum = 0;
+
+	if (!b)
+		return (0);
+
+	while (b[i])
+		i++;
+
+	i--;
+	while (b[i])
+	{
+		if (b[i] != '1' && b[i] != '0')
+			return (0);
+
+		sum += (b[i] - '0') * (1 << mul);
+		mul++;
+		b--;
+	}
+
+	return (sum);
+}
