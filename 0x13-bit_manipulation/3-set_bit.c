fa0d68a5eaffe5725b44fd87b292f8a3a58da25f
wescottsharples <wesc@ttsharpl.es>
2018-11-02 04:06:14 +0000

    Add description of function and perenthesis around return

--- 0x13-bit_manipulation/3-set_bit.c
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -1,9 +1,16 @@
 #include "holberton.h"
 
+/**
+ * set_bit - Sets the value of a bit to 1 at a given index.
+ * @n: the number to be analyzed
+ * @index: the index of bit to be set
+ *
+ * Return: 1 on success, -1 on failure.
+ */
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (sizeof(n) * 8 < index)
-		return -1;
+		return (-1);
 
 	*n = *n | 1 << index;
 	return (1);
