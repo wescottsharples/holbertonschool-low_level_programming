8ebf567027ffd4ebfbb06f311f229678e39f2039
wescottsharples <wesc@ttsharpl.es>
2018-12-09 23:25:07 +0000

    Add description for function

--- 0x16-doubly_linked_lists/0-print_dlistint.c
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * print_dlistint - function prints elements of doubly linked list
+ * @h: the head node of linked list
+ *
+ * Return: The number of elements in the list.
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *current;
