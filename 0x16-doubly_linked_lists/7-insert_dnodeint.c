5380476928da5e392f2e19372ec667cf4b511fe7
wescottsharples <wesc@ttsharpl.es>
2018-12-10 00:54:32 +0000

    Add function for insertion of node

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -3,7 +3,40 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - inserts a new node a a given position
+ * insertion - inserts a new node
+ * @current: the node to be inserted before
+ * @n: int to be stored inside newly created node
+ *
+ * Return: Pointer to the newly created node on success, NULL on failure.
+ */
+dlistint_t *insertion(dlistint_t *current, int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+
+	if (current)
+	{
+		new->next = current;
+		if (current->prev)
+		{
+			new->prev = current->prev;
+			current->prev->next = new;
+			current->prev = new;
+		}
+	}
+
+	return (new);
+}
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
  * @h: double pointer to the head node of linked list
  * @idx: numerical index of node to be inserted starting from 0
  * @n: int to be stored inside newly created node
@@ -22,15 +55,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		if (count == idx)
 		{
-			new = malloc(sizeof(dlistint_t));
-			if (!new)
-				return (NULL);
+			new = insertion(current, n);
 
-			new->n = n;
-			new->next = current;
-			new->prev = current->prev;
-			current->prev->next = new;
-			current->prev = new;
 			if (idx == 0)
 				*h = new;
 
@@ -40,6 +66,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		current = current->next;
 		count++;
 	}
+
+	if (count == idx)
+		return (insertion(current, n));
 
 	return (NULL);
 }
