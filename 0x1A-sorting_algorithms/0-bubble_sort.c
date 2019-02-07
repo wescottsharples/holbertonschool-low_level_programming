2a9c5e9d1746112902bc3c48ffbfa0bcaefac9bc
wescottsharples <wesc@ttsharpl.es>
2019-02-07 01:22:40 +0000

    Add bubble sort function

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -1,18 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "sort.h"
 
+/**
+ * bubble_sort - sorts array using bubble sort
+ * and prints array after each swap
+ *
+ * @array: array to be sorted
+ * @size: the size of array
+ *
+ * Return: Nothing
+ */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i;
 	int tmp;
+	char flag = 1;
 
-	for (i = 0; i < size - 1; i++)
+	while (flag)
 	{
-		if (array[i] > array[i + 1])
+		flag = 0;
+		for (i = 0; i < size - 1; i++)
 		{
-			tmp = array[i];
-			array[i] = array[i + 1];
-			array[i + 1] = tmp;
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				print_array(array, size);
+				flag = 1;
+			}
 		}
 	}
+}
