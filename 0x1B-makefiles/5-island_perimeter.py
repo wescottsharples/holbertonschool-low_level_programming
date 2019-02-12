cb5a71c9493590940326b63d5c9d14c5b54a8614
wescottsharples <wesc@ttsharpl.es>
2019-02-12 02:12:19 +0000

    Fix conditional check for edges

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -9,18 +9,17 @@ Contains function to count perimeter of an island
 def island_perimeter(grid):
     """ Returns the island's perimeter """
     perimeter = 0
-    if len(grid) - 1 == 0 or len(grid[0]) -1 == 0:
+    if len(grid) - 1 == 0 or len(grid[0]) - 1 == 0:
         return 0
-    matrix = grid.copy()
     for y in range(len(grid)):
-        for x in range(len(grid[y])):
+        for x in range(len(grid[0])):
             if grid[y][x] == 1:
-                if y == len(grid[0]) - 1 or grid[y - 1][x] == 0:
+                if y == 0 or grid[y - 1][x] == 0:
                     perimeter += 1
-                if x == len(grid) - 1 or grid[y][x + 1] == 0:
+                if x == len(grid[0]) - 1 or grid[y][x + 1] == 0:
                     perimeter += 1
-                if y == len(grid[0]) - 1 or grid[y + 1][x] == 0:
+                if y == len(grid) - 1 or grid[y + 1][x] == 0:
                     perimeter += 1
-                if x == len(grid) - 1 or grid[y][x - 1] == 0:
+                if x == 0 or grid[y][x - 1] == 0:
                     perimeter += 1
     return perimeter
