5d26653ea1159cc853550dbfb7409b7578b05e8c
wescottsharples <wesc@ttsharpl.es>
2019-03-13 23:58:53 +0000

    Function adds right node

---
+++ 0x1C-binary_trees/2-binary_tree_insert_right.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - creates a binary tree right node
+ *
+ * @parent: pointer to the parent node of the node to create
+ * @value: the value to insert in new node
+ *
+ * Return: pointer to new node on success, NULL on failure
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = NULL;
+
+	if (!parent)
+		return (NULL);
+
+	node = malloc(sizeof(binary_tree_t));
+	if (!node)
+		return (NULL);
+
+	node->n = value;
+	node->parent = parent;
+	node->left = NULL;
+	node->right = NULL;
+
+	if (parent->right)
+	{
+		node->right = parent->right;
+		parent->right->parent = node;
+		parent->right = node;
+	}
+	else
+		parent->right = node;
+
+	return (node);
+}
