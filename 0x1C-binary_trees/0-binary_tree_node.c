1f467e251d94eac849962c61dd58fdadcf58d375
wescottsharples <wesc@ttsharpl.es>
2019-03-13 23:04:18 +0000

    Function adds new node to parent

---
+++ 0x1C-binary_trees/0-binary_tree_node.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a binary tree node
+ *
+ * @parent: pointer to the parent node of the node to create
+ * @value: the value to insert in new node
+ *
+ * Return: pointer to new node on success, NULL on failure
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *child = NULL;
+
+	if (parent == NULL)
+		return (NULL);
+
+	child = malloc(sizeof(binary_tree_t));
+	child->n = value;
+	child->parent = parent;
+	child->left = NULL;
+	child->right = NULL;
+
+	return (child);
+}
