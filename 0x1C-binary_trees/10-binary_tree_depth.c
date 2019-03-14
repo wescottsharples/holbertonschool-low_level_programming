7e1ee4ee207a9d0eac377a5fb586fdd4b6031515
wescottsharples <wesc@ttsharpl.es>
2019-03-14 04:31:41 +0000

    Function finds the depth of node in binary tree

---
+++ 0x1C-binary_trees/10-binary_tree_depth.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - finds the depth of node in binary tree
+ *
+ * @tree: pointer to the node to find depth of
+ *
+ * Return: depth of node, otherwise 0
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth = 0;
+
+	if (!tree)
+		return (0);
+
+	while(tree->parent)
+	{
+		depth++;
+		tree = tree->parent;
+	}
+
+	return (depth);
+}
