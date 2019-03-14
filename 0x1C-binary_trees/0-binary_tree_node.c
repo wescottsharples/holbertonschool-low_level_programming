2c7a4f12ffc9b4af1dc16e85f82672dfe67f7611
wescottsharples <wesc@ttsharpl.es>
2019-03-14 05:35:46 +0000

    Fix extra sorting logic

--- 0x1C-binary_trees/0-binary_tree_node.c
+++ 0x1C-binary_trees/0-binary_tree_node.c
@@ -13,20 +13,13 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node = malloc(sizeof(binary_tree_t));
 
+	if (!node)
+		return (NULL);
+
 	node->n = value;
+	node->parent = parent;
 	node->left = NULL;
 	node->right = NULL;
-
-	if (parent)
-	{
-		node->parent = parent;
-		if (value <= parent->n)
-			parent->left = node;
-		else if (value > parent->n)
-			parent->right = node;
-	}
-	else
-		parent = NULL;
 
 	return (node);
 }
