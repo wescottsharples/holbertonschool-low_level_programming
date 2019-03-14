0e0099f76e6a0a6e8bd103f34e5b1ce8f1cfdf76
wescottsharples <wesc@ttsharpl.es>
2019-03-14 08:17:45 +0000

    Add correct function for height

--- 0x1C-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1C-binary_trees/16-binary_tree_is_perfect.c
@@ -2,6 +2,21 @@
 #include "binary_trees.h"
 
 /**
+ * height - recursively finds the height of a binary tree
+ *
+ * @tree: pointer to the root node of binary tree
+ *
+ * Return: the height of the binary tree
+ */
+size_t height(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (-1);
+
+	return (max(height(tree->left) + 1, height(tree->right) + 1));
+}
+
+/**
  * max - returns the maximum of two unsigned ints
  *
  * @a: the first unsigned int
@@ -14,21 +29,6 @@ size_t max(size_t a, size_t b)
 	if (a > b)
 		return (a);
 	return (b);
-}
-
-/**
- * binary_tree_height - finds height if tree exists
- *
- * @tree: pointer to the root node of binary tree
- *
- * Return: the height of the binary tree, otherwise 0
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	if (!tree)
-		return (0);
-
-	return (height(tree));
 }
 
 /**
