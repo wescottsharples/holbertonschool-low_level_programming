fa57780ef78e3cd216453920c07c1e8a23c96775
wescottsharples <wesc@ttsharpl.es>
2019-03-14 04:24:16 +0000

    Add condition for NULL tree

--- 0x1C-binary_trees/9-binary_tree_height.c
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -2,6 +2,21 @@
 #include "binary_trees.h"
 
 /**
+ * height - recursively finds the height of a binary tree
+ *
+ * @tree: pointer to the root node of binary tree
+ *
+ * Return: the height of the binary tree
+ */
+size_t height(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (-1);
+
+	return (max(height(tree->left) + 1, height(tree->right) + 1));
+}
+
+/**
  * max - returns the maximum of two unsigned ints
  *
  * @a: the first unsigned int
@@ -17,17 +32,16 @@ size_t max(size_t a, size_t b)
 }
 
 /**
- * binary_tree_height - recursively finds the height of a binary tree
+ * binary_tree_height - finds height if tree exists
  *
  * @tree: pointer to the root node of binary tree
  *
- * Return: the height of the binary tree
+ * Return: the height of the binary tree, otherwise 0
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (!tree)
-		return (-1);
+		return (0);
 
-	return (max(binary_tree_height(tree->left) + 1,
-				binary_tree_height(tree->right) + 1));
+	return(height(tree));
 }
