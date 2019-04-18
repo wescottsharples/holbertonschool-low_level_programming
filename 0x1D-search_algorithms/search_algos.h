42374d00f8fc914583da09c76d4620d8645a085c
Wescott Sharples <wesc@ttsharpl.es>
2019-04-18 17:49:07 +0000

    Header containing func protos

---
+++ 0x1D-search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef __SEARCH_ALGOS__
+#define __SEARCH_ALGOS__
+
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif /* __SEARCH_ALGOS__ */
