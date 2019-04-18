a46b637f3a57111e082b09ca6e248da79598712d
Wescott Sharples <wesc@ttsharpl.es>
2019-04-18 17:54:15 +0000

    Function searches for value in arr of ints using linear search

---
+++ 0x1D-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - Searches for a vlaue using the Linear Search algo
+ *
+ * @array: ptr to first element  of the arr to search
+ * @size: the number of elements in array
+ * @value: the value to search for
+ *
+ * Return: The first index of value on success, otherwise -1
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (!array)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
+}
